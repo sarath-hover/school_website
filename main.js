72ad55d5214c84e85eac8a7f9a213f3de9d8cfbe
sarath-hover <sarathhover47@gmail.com>
2023-06-14 17:15:38 +0530

    courses section starts on jun14th 2023

---
+++ main.js
@@ -0,0 +1,26 @@
+//  Change navbar style on scroll 
+
+window.addEventListener('scroll', () =>{
+    document.querySelector('nav').classList.toggle('window-scroll', window.scrollY > 0)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
